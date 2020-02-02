10288e0a42710e612aad9bd3b5ba290489f3fd3d
aminya <aminyahyaabadi74@gmail.com>
2020-02-01 19:43:19 -0600

    Test for allsubtypes

--- test/specific.jl
+++ test/specific.jl
@@ -1,4 +1,4 @@
-@testset "sepecific" begin
+@testset "subtypes" begin
     abstract type A end
     abstract type B <:A end
     abstract type C <:B end
@@ -7,6 +7,20 @@
            print("vector method")
     end
 
-    methods(foo)
+    @test length(methods(foo)) == 1
+
+end
+
+
+@testset "allsubtypes" begin
+    abstract type A end
+    abstract type B <:A end
+    abstract type C <:B end
+
+    @specific function foo(a, b::allsubtypes(A), c::T) where {T<:Int64}
+           print("vector method")
+    end
+
+    @test length(methods(foo)) == 3
 
 end
