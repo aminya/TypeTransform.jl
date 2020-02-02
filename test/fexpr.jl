9bed08889323e16c5bc2bcc1048bb2fe271d9e36
aminya <aminyahyaabadi74@gmail.com>
2020-02-01 18:50:40 -0600

    Update fexpr tests

--- test/fexpr.jl
+++ test/fexpr.jl
@@ -1,9 +1,15 @@
 @testset "fexpr" begin
 
-    fexpr = :( function foo(a, b::T1, c::T2) where {T1<:A} where {T2<:Int64}
+    fexpr = :( function foo(a, b::T1, c::T2) where {T1<:AbstractArray} where {T2<:Int64}
            print("vector method")
      end)
     f, args, wherestack, body = unwrap_fun(fexpr, true, true)
     fexpr1 = wrap_fun(f, args, wherestack, body)
+
+    eval(fexpr1)
+
+    methods(foo)
+
+    # @test fexp1 == fexpr
 
 end
