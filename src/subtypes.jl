6cc0f70529793e521a80b1e1697725ae422e5a9d
aminya <aminyahyaabadi74@gmail.com>
2020-02-01 18:30:46 -0600

    allsubtypes

---
+++ src/subtypes.jl
@@ -0,0 +1,21 @@
+################################################################
+import InteractiveUtils.subtypes
+
+function allsubtypes(T::Type)
+    t = Set{Type}()
+    push!(t, T) # include itself
+    allsubtypes.(subtypes(T), Ref(t))
+    t
+end
+function allsubtypes(T, t)
+    # T is an element
+    # recursive method
+    push!(t, T)
+    if isempty(subtypes(T))
+        return
+    else
+        allsubtypes.(subtypes(T), Ref(t))
+    end
+end
+
+allsubtypes(T::Symbol) = allsubtypes(Core.eval(Main, T))#convert Symbol to Type
