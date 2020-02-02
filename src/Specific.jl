bd35a8da486538f5e088734dd164b4b1620a57c7
aminya <aminyahyaabadi74@gmail.com>
2020-02-02 03:11:48 -0600

    Update readme

--- src/Specific.jl
+++ src/Specific.jl
@@ -7,9 +7,10 @@ include("subtypes.jl")
 
 """
     @specific
-
-Define specific methods for all of subtypes of type.
+    
+Define specific methods for all of subtypes of type (fix ambiguity error!).
 ```julia
+using Specific
 abstract type A end
 abstract type B <:A end
 abstract type C <:B end
@@ -18,17 +19,34 @@ abstract type C <:B end
     println("a new method")
 end
 ```
+```julia
+julia> methods(foo)
+# 3 methods for generic function "foo":
+[1] foo(a, b::C) in Main at none:2
+[2] foo(a, b::B) in Main at none:2
+[3] foo(a, b::A) in Main at none:2
+```
+You can use `subtypes` instead of `allsubtypes`, which defines methods only for the direct subtypes.
 
 
-Supports functions that have `where`
+# Why? To fix ambiguity error!
+If you run the following program
+```julia
+abstract type A end
+abstract type B <:A end
+
+# my general vector method
+foo(a::Vector, b::Type{<:A}) = print("vector method")
+
+# my special B mwthod
+foo(a, b::Type{B}) = print("B method")
+```
+`foo([1,2], B)` will give an ambiguity error, while if you use `allsubtypes`, you can fix the issue.
 
 ```julia
-@specific function foo(a, b::allsubtypes(A), c::T) where {T<:Int64}
-    println("a new method")
-end
+# my general vector method
+@specific foo(a::Vector, b::allsubtypes(A)) = print("vector method")
 ```
-
-You can use just `subtypes` instead of `allsubtypes`, and in this situation only method for direct subtypes are defined.
 
 """
 macro specific(fexpr::Expr)
